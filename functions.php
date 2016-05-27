f0cc7c86d8b83c635381c436a05ccce29b62fb57
Vincent Kristoffer Canete <vincentkristofferzcanete@gmail.com>
2016-05-28 00:54:17 +0800

    initial commit

---
+++ functions.php
@@ -0,0 +1,58 @@
+<?php
+require_once('lib/clean.php'); // do all the cleaning and enqueue here
+require_once('lib/enqueue-style.php');
+require_once('lib/foundation.php'); // load Foundation specific functions like top-bar
+require_once('lib/nav.php'); // filter default wordpress menu classes and clean wp_nav_menu markup
+
+
+if( ! function_exists( 'reverie_theme_support' ) ) {
+    function reverie_theme_support() {
+        // Add language supports.
+        load_theme_textdomain('reverie', get_template_directory() . '/lang');
+
+        // Add post thumbnail supports. http://codex.wordpress.org/Post_Thumbnails
+        add_theme_support('post-thumbnails');
+        // set_post_thumbnail_size(150, 150, false);
+
+        // rss thingy
+        add_theme_support('automatic-feed-links');
+
+        // Add menu support. http://codex.wordpress.org/Function_Reference/register_nav_menus
+        add_theme_support('menus');
+        register_nav_menus(array(
+            'primary' => __('Primary Navigation', 'reverie'),
+        ));
+
+        // Add custom background support
+        add_theme_support( 'custom-background',
+            array(
+                'default-image' => '',  // background image default
+                'default-color' => '', // background color default (dont add the #)
+                'wp-head-callback' => '_custom_background_cb',
+                'admin-head-callback' => '',
+                'admin-preview-callback' => ''
+            )
+        );
+    }
+}
+add_action('after_setup_theme', 'reverie_theme_support'); /* end Reverie theme support */
+
+// create widget areas: sidebar, footer
+$sidebars = array('Sidebar');
+foreach ($sidebars as $sidebar) {
+    register_sidebar(array('name'=> $sidebar,
+    	'id' => 'Sidebar',
+        'before_widget' => '<div id="%1$s" class="panel widget %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4 class="widget-title">',
+        'after_title' => '</h4>'
+    ));
+}
+
+// return entry meta information for posts, used by multiple loops, you can override this function by defining them first in your child theme's functions.php file
+if ( ! function_exists( 'reverie_entry_meta' ) ) {
+    function reverie_entry_meta() {
+        echo '<span class="byline author">'. __('Written by', 'reverie') .' <a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'. get_the_author() .', </a></span>';
+        echo '<time class="updated" datetime="'. get_the_time('c') .'" pubdate>'. get_the_time('F jS, Y') .'</time>';
+    }
+};
