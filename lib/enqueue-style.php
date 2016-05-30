b36f6b1acdcd0824bf1915173a4bf94f6d407764
Vincent Kristoffer Canete <vincentkristofferzcanete@gmail.com>
2016-05-30 16:42:44 +0800

    Update enqueue-style.php

--- lib/enqueue-style.php
+++ lib/enqueue-style.php
@@ -1,15 +1,11 @@
 <?php
-/*********************
-Enqueue the proper CSS
-if you use Sass.
-*********************/
 if( ! function_exists( 'reverie_enqueue_style' ) ) {
 	function reverie_enqueue_style()
 	{
 		// foundation stylesheet
 		wp_register_style( 'reverie-foundation-stylesheet', get_stylesheet_directory_uri() . '/dist/styles/main.css', array(), '' );
 
-		// Register the main style
+		// Register the app style
 		wp_register_style( 'reverie-stylesheet', get_stylesheet_directory_uri() . '/assets/styles/app.css', array(), '', 'all' );
 		
 		wp_enqueue_style( 'reverie-foundation-stylesheet' );
