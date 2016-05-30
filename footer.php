0911c05502df18b5688c60902be388906611f3b9
Vincent Kristoffer Canete <vincentkristofferzcanete@gmail.com>
2016-05-30 15:02:41 +0800

    updated footer

--- footer.php
+++ footer.php
@@ -13,11 +13,6 @@
 			<?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list')); ?>
 		</div>
 	</div>
-	<div class="row love-reverie">
-		<div class="large-12 columns">
-			<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php _e('Made with Love in','reverie'); ?> <a href="http://themefortress.com/reverie/" rel="nofollow" title="Reverie Framework">Reverie</a>.</p>
-		</div>
-	</div>
 </footer>
 
 <?php wp_footer(); ?>
