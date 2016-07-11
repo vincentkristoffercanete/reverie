de77f30f71173760b6b5e13f3f57d91a161783a4
Vincent Kristoffer Canete <vincentkristofferzcanete@gmail.com>
2016-07-11 09:58:23 +0800

    Change theme css to scss

--- lib/enqueue-style.php
+++ lib/enqueue-style.php
@@ -3,12 +3,7 @@ if( ! function_exists( 'reverie_enqueue_style' ) ) {
 	function reverie_enqueue_style()
 	{
 		// foundation stylesheet
-		wp_register_style( 'reverie-foundation-stylesheet', get_stylesheet_directory_uri() . '/dist/styles/main.css', array(), '' );
-
-		// Register the app style
-		wp_register_style( 'reverie-stylesheet', get_stylesheet_directory_uri() . '/dist/styles/app.css', array(), '', 'all' );
-		
-		wp_enqueue_style( 'reverie-foundation-stylesheet' );
+		wp_register_style( 'reverie-stylesheet', get_stylesheet_directory_uri() . '/dist/styles/main.css', array(), '' );
 		wp_enqueue_style( 'reverie-stylesheet' );
 		
 	}
